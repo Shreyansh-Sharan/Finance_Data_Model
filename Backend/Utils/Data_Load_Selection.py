875115bf5df1d18ac038c1fed75634ebefc738ee
Shreyansh Sharan <shreyansh.sharan@Polestarllp.local>
2023-09-02 01:34:57 +0530

    Added the functionality to fetch the Stocks request data and add it into the central repository of stock list

--- Backend/Utils/Data_Load_Selection.py
+++ Backend/Utils/Data_Load_Selection.py
@@ -1,25 +1,35 @@
 import pandas as pd
 import csv
+import logging as log
+
 # data = pd.read_csv("C:\\\\Users\\shreyansh.sharan\\Desktop\\GitHub\\Raw_Data\\total_requested_stocks.csv", delimiter= ',')
 # data = data.values.tolist()
 # print(data[1])
 
-with open("C:\\\\Users\\shreyansh.sharan\\Desktop\\GitHub\\Raw_Data\\total_requested_stocks.csv", 'r') as read_obj:
+read_obj = open("C:\\\\Users\\shreyansh.sharan\\Desktop\\GitHub\\Backend\\Raw_Data\\total_requested_stocks.csv") 
 
-    csv_reader = csv.reader(read_obj)
-    list_of_csv = list(csv_reader)
+csv_reader = csv.reader(read_obj)
+list_of_csv = list(csv_reader)[0]
 
-print(list_of_csv[0])
+print(list_of_csv)
 
-def data_load_selection(user_list=['GOOGL','AMZN']):
+def data_load_selection(user_list):
+    print("Hey! Proceeding with the extraction")
     for i in user_list:
-        if i in list_of_csv[0]:
-            #data_load_selection(i)
+        if i in list_of_csv:
             print("Hellos")
             pass
         else:
+            print(list_of_csv)
+            list_of_csv.append(i)
             #finance_full_load(i)
+            print(list_of_csv)
+            with open('C:\\\\Users\\shreyansh.sharan\\Desktop\\GitHub\\Backend\\Raw_Data\\total_requested_stocks.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(list_of_csv)
             pass
+            return "failed"
 
+# print(data_load_selection())
 
 
--- fast_api_master.py
+++ fast_api_master.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import asyncio
+import Backend.Utils.Data_Load_Selection as stocks_list_update
 
 class User_input(BaseModel):
-    x : []
+    stocks_input_payload : list
 
 app = FastAPI()
 
-@app.post("/fetch")
-def data_load_selection(input:User_input):
-    result = data_load_selection(input.x)
-    return result
+@app.post("/fetchInformation")
+async def fetchInformation(user_input:User_input):
+    # print(user_input.stocks_input_payload)
+    stocks_list_update.data_load_selection(user_input.stocks_input_payload)
+    return {"result":user_input}
+
